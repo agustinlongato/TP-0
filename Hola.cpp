ef5509581f79efcecbd55d932f0daccd22b81d36
agustinlongato <aguslongato@hotmail.com>
2015-04-09 01:28:55 -0300

    Update Hola.cpp

--- Hola.cpp
+++ Hola.cpp
@@ -1,6 +1,10 @@
+/* Programa que muestra en la pantalla el mensaje escrito abajo en el codigo
+ * Agustin Longato
+ * 09/04/2015
+*/
 #include <iostream>
 using namespace std;
 int main ()
 {
-	cout<< "Hola AED, TP n°0";
+	cout<< "Hola AED, TP nÂ°0";
 }
